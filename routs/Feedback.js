c910eb4ec564a7eaa3640ed4989bb5dbcae941ba
DhananjayaLakshan <abey.dhananjaya@gmail.com>
2023-10-28 02:02:23 +0530

    delete route is added

--- routs/Feedback.js
+++ routs/Feedback.js
@@ -53,4 +53,22 @@ router.post("/getFeedbackByName", async (req, res) => {
 
 })
 
+router.delete("/deleteFeedback/:id", async (req, res) => {
+
+    const id = req.params.id // Use req.params to access the roomID from the URL
+
+    try {
+        const feedback = await cus_feed.findOneAndDelete({ _id: id })
+
+        if (feedback) {
+            res.send('feedback are deleted')
+        } else {
+            res.status(404).json({ error: 'feedback not found' })
+        }
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({ error: 'Internal server error' })
+    }
+})
+
 module.exports = router
